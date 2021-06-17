8f55cee0245a3abd2e62ae867e99e645b0803ed2
luliangyu <luliangyu@seewintech.com>
2021-06-17 16:45:12 +0800

    轮询模式

---
+++ src/main/java/com/l/consumer/WorkConsumer.java
@@ -0,0 +1,35 @@
+package com.l.consumer;
+
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * 工作队列模式
+ *
+ * @author luliangyu
+ * @date 2021-06-17 16:31
+ */
+@Component
+public class WorkConsumer {
+    private final RabbitTemplate rabbitTemplate;
+
+    public WorkConsumer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @RabbitListener(queuesToDeclare = @Queue("work_queue"))
+    public void workQueue(String message) {
+        System.out.println("WorkConsumer.workQueue = " + message);
+    }
+
+    @RabbitListener(queuesToDeclare = @Queue("work_queue"))
+    public void workQueue2(String message) {
+        System.out.println("WorkConsumer.workQueue2 = " + message);
+    }
+
+    public void send(String message) {
+        rabbitTemplate.convertAndSend("work_queue", message);
+    }
+}
--- src/main/java/com/l/controller/RabbitController.java
+++ src/main/java/com/l/controller/RabbitController.java
@@ -1,7 +1,9 @@
 package com.l.controller;
 
+import com.l.consumer.WorkConsumer;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rabbit")
 public class RabbitController {
     private final RabbitTemplate rabbitTemplate;
+    private final WorkConsumer workConsumer;
 
-    public RabbitController(RabbitTemplate rabbitTemplate) {
+    public RabbitController(RabbitTemplate rabbitTemplate, WorkConsumer workConsumer) {
         this.rabbitTemplate = rabbitTemplate;
+        this.workConsumer = workConsumer;
     }
 
     @GetMapping("/sendQueue")
     public void sendQueue(String msg) {
         rabbitTemplate.convertAndSend("simple_queue", msg);
-        return;
+    }
+
+    @GetMapping("/sendWorkQueue/{message}")
+    public void sendWorkQueue(@PathVariable String message) {
+        workConsumer.send(message);
     }
 }
